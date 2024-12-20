9e2d3845fcc6d5f9a5f63b011fafe5a243681c36
TEJU D <111564230+tejudharanesh@users.noreply.github.com>
2024-12-20 22:29:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+  
     proxy: {
       "/api": {
         target: "https://twitter-j89n.onrender.com",
