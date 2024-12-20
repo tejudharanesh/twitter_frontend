6a5a69cfd4059ddaf53dbf8fbc0f625e4907024e
teju_d Github <tejudharanesh1234@gmail.com>
2024-12-21 00:01:43 +0530

    refer

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://twitter-j89n.onrender.com",
-        changeOrigin: true,
-      },
-    },
-  },
 });
