48363c6174db9d48edf78a329f4ad389fce39b3d
teju_d Github <tejudharanesh1234@gmail.com>
2025-01-20 09:50:51 +0530

    merged frontend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_ENDPOINT = "http://localhost:5000";
+const API_ENDPOINT = "https://localhost:5000";
 console.log("API Endpoint:", API_ENDPOINT);
 
 /**
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://twitter-j89n.onrender.com",
+        target: "https:localhost:5000",
         changeOrigin: true,
       },
     },
